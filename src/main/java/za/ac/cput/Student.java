a5b5652974a7c0c60a6e0d3d8272ceccad0b39f4
Sisipho Mvimvi <217070671@mycput.ac.za>
2021-05-14 22:15:15 +0200

    Assignment2

---
+++ src/main/java/za/ac/cput/Student.java
@@ -0,0 +1,28 @@
+package za.ac.cput;
+
+/*Name: Sisipho Mvimvi
+  This Program is to test Collection, Map, List and Set test
+ */
+
+
+import java.util.*;
+
+public class Student {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Collection<String> studentCollection = new ArrayList<>();
+        List<String> studentList = new ArrayList<>();
+        Map<Integer, String> studentMap = new HashMap<>();
+        Set<String> studentSet = new HashSet<>();
+
+
+
+    }
+}
+
+
+
+
+
+
+
